e6d77a941c55fba85a5371bf414a4f2cd2ae8e4d
vikas-saini-7 <vikas289888@gmail.com>
2024-01-12 02:48:48 +0530

    added /get and /get/:id in course

--- routes/course.js
+++ routes/course.js
@@ -2,8 +2,37 @@ const express = require("express");
 const Course = require("../models/Course");
 const router = express.Router();
 
+// GET with ID
+router.get('/:courseId', async (req, res) => {
+    const { courseId } = req.params;
+
+    try {
+        const course = await Course.findById(courseId);
+        
+        if (!course) {
+        return res.status(404).json({ error: 'Course not found' });
+        }
+        
+        res.json(course);
+    } catch (error) {
+        console.error('Error fetching course:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// GET all course general
+router.get('/', async (req, res) => {
+    try {
+        const courses = await Course.find();
+        res.json(courses);
+    } catch (error) {
+        console.error('Error fetching courses:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 router.post("/", (req, res) =>{
-    if(req.query.role !== "student"){
+    if(req.body.role !== "student"){
         return res.status(403).json({error: "Unauthorized access"});
     }
 
